9f1951b04f2d02abb44932c927c396ad29ead58d
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2003-01-11 16:24:07 +0000

    TCP and UNIX support.
    git-svn-id: file:///home/sune/furrfu/repositories/public/Sheared@18 5646265b-94b7-0310-9681-9501d24b2df7

---
+++ sheared/internet/socket.py
@@ -0,0 +1,68 @@
+# vim:nowrap:textwidth=0
+
+import socket, sys
+from sheared.python import coroutine
+
+class Port:
+    def __init__(self, reactor, factory, address, family, type, backlog):
+        self.factory = factory
+        self.address = address
+        self.backlog = backlog
+        self.family = family
+        self.type = type
+        self.reactor = reactor
+
+    def listen(self):
+        self.socket = socket.socket(self.family, self.type)
+        sock.socket = reactor.prepareFile(self.socket)
+        self.reactor.bind(self.socket, self.address)
+        self.reactor.listen(self.socket, self.backlog)
+        self.coroutine = coroutine.Coroutine(self._run)
+        self.reactor.addCoroutine(self.coroutine, ())
+
+    def _run(self):
+        while 1:
+            fd, addr = self.reactor.accept(self.socket)
+            transport = self.reactor.createTransport(fd, addr)
+            co = self.factory.buildCoroutine(transport)
+            self.reactor.addCoroutine(co, ())
+
+class TCPPort(Port):
+    def __init__(self, reactor, factory, address, backlog=5):
+        Port.__init__(self, reactor, factory, address, socket.AF_INET, socket.SOCK_STREAM, backlog)
+
+class UNIXPort(Port):
+    def __init__(self, reactor, factory, address, backlog=5):
+        Port.__init__(self, reactor, factory, address, socket.AF_UNIX, socket.SOCK_STREAM, backlog)
+
+    
+class Client:
+    def __init__(self, reactor, factory, addr, from_addr, family, type):
+        self.factory = factory
+        self.from_address = from_addr
+        self.to_address = addr
+        self.family = family
+        self.type = type
+        self.reactor = reactor
+        
+    def connect(self):
+        self.socket = socket.socket(self.family, self.type)
+        self.socket = self.reactor.prepareFile(self.socket)
+        if not self.from_address is None:
+            self.socket.bind(self.from_address)
+        transport = self.reactor.createTransport(self.socket, self.to_address)
+        coroutine = self.factory.buildCoroutine(transport)
+        try:
+            self.reactor.connect(self.socket, self.to_address)
+            self.reactor.addCoroutine(self.coroutine, (None,))
+        except:
+            self.reactor.addCoroutine(self.coroutine, (sys.exc_info(),))
+
+class TCPClient(Client):
+    def __init__(self, reactor, factory, address, from_addr):
+        Client.__init__(self, reactor, factory, address, from_addr, socket.AF_INET, socket.SOCK_STREAM)
+
+class UNIXClient(Client):
+    def __init__(self, reactor, factory, address, backlog):
+        Client.__init__(self, reactor, factory, address, from_addr, socket.AF_UNIX, socket.SOCK_STREAM)
+         
