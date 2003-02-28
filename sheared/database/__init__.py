2e9453f959c02818c918ab493531de1f9e1a7289
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2003-02-28 07:55:57 +0000

    Update __init__ modules to reflect postgresql module move.
    git-svn-id: file:///home/sune/furrfu/repositories/public/Sheared@55 5646265b-94b7-0310-9681-9501d24b2df7

--- sheared/database/__init__.py
+++ sheared/database/__init__.py
@@ -1,6 +1,1 @@
-from sheared.database.error import *
-from sheared.database import pool
-from sheared.database import dummy
-from sheared.database import postgresql
-__all__ = ['pool', 'dummy', 'postgresql', 'DatabaseError',
-    'ProtocolError', 'OperationalError', 'ProgrammingError']
+__all__ = ['pool', 'dummy', 'error']
--- sheared/protocol/__init__.py
+++ sheared/protocol/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['basic', 'echo', 'http']
+__all__ = ['basic', 'echo', 'http', 'postgresql']
