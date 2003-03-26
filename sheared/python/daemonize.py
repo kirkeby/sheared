6b15f6e08a71ff5e676af56f31486ac1251d85ba
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2003-03-26 17:56:29 +0000

    Add normally function.
    git-svn-id: file:///home/sune/furrfu/repositories/public/Sheared@86 5646265b-94b7-0310-9681-9501d24b2df7

--- sheared/python/daemonize.py
+++ sheared/python/daemonize.py
@@ -1,7 +1,24 @@
 from __future__ import nested_scopes
-import os, sys, errno
 
-def background(chdir=1):
+import os, sys, errno, atexit, signal
+
+from sheared import reactor
+
+def normally(pidfile=1, logfile=1):
+    if pidfile == 1:
+        pidfile = sys.argv[0] + '.pid'
+    if logfile == 1:
+        logfile = sys.argv[0] + '.log'
+    
+    background(chdir=0)
+    handle_signals()
+
+    if pidfile:
+        writepidfile(pidfile)
+    if logfile:
+        openstdlog(logfile)
+
+def background(chdir=1, close=1):
     # do the UNIX double-fork magic, see Stevens' "Advanced 
     # Programming in the UNIX Environment" for details (ISBN 0201563177)
     if os.fork():
@@ -21,12 +38,23 @@ def background(chdir=1):
         sys.exit(0) 
 
     # close all open file-descriptors
-    closeall()
+    if close:
+        closeall()
 
 def writepidfile(path):
     f = open(path, 'w')
     f.write('%d\n' % os.getpid())
     f.close()
+
+    def unlink():
+        os.unlink(path)
+    atexit.register(unlink)
+
+def handle_signals():
+    def stop(signum, frame):
+        reactor.current.shutdown()
+    signal.signal(signal.SIGINT, stop)
+    signal.signal(signal.SIGTERM, stop)
 
 def closerange(r):
     for fd in r:
@@ -55,8 +83,12 @@ def openstdio(stdin='/dev/zero', stdout='/dev/null', stderr='/dev/null'):
 
     sys.stdin = open(stdin, 'r')
     sys.stdout = open(stdout, 'w')
-    sys.stderr = open(stderr, 'w')
+    if stderr == stdout:
+        sys.stderr = sys.stdout
+        os.dup2(1, 2)
+    else:
+        sys.stderr = open(stderr, 'w')
 
     assert sys.stdin.fileno() == 0
     assert sys.stdout.fileno() == 1
-    assert sys.stderr.fileno() == 2
+    assert sys.stderr.fileno() == 2 or sys.stderr is sys.stdout
