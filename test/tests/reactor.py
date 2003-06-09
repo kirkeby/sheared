463d6557df5d336d1bd5237995cdd5edc2ae86d7
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2003-06-09 08:24:12 +0000

    Updated most of the reactor test-cases to work on the new code.
    git-svn-id: file:///home/sune/furrfu/repositories/public/Sheared@97 5646265b-94b7-0310-9681-9501d24b2df7

--- test/tests/reactor.py
+++ test/tests/reactor.py
@@ -4,197 +4,175 @@
 import unittest
 import socket, os, time, errno, commands, random
 
-from sheared.reactor import selectable
-from sheared.python import coroutine
-
 class ReactorTestCaseMixin:
     """Test a reactor."""
 
     def setUp(self):
-        self.reactor.reset()
-
-    def testCanReset(self):
-        """Test that the reactor can be reset."""
-        self.reactor.run()
-        self.reactor.reset()
-        self.reactor.run()
+        self.reactor = self.klass()
 
     def testNothingToDo(self):
         """Test that the reactor cleanly exits when there is nothing to do."""
         self.reactor.run()
 
-    def singleCoroutineRun(self, f):
-        try:
-            co = coroutine.Coroutine(f)
-            self.reactor.addCoroutine(co, (self.reactor,))
-            self.reactor.run()
-            return getattr(self.reactor, 'result', None)
-        except coroutine.CoroutineFailed, ex:
-            raise ex[0].exc_info[0], ex[0].exc_info[1], ex[0].exc_info[2]
-
     def testRead(self):
         """Test reading from an open file."""
-        def f(reactor):
+        def f():
             fd = os.open('test/tests/reactor.py', os.O_RDONLY)
-            fd = reactor.prepareFile(fd)
-            lines = reactor.read(fd, 4096).split('\n')
+            fd = self.reactor.prepareFile(fd)
+            lines = self.reactor.read(fd, 4096).split('\n')
             self.assertEquals(lines[0], '# First line.')
-        self.singleCoroutineRun(f)
+        self.reactor.createtasklet(f)
+        self.reactor.run()
         
     def testBadWrite(self):
         """Test that the reactor works given bad input to write."""
-        def f(reactor):
-            f = reactor.open('/dev/null', 'w')
+        def f():
+            f = self.reactor.open('/dev/null', 'w')
             try:
-                reactor.write(f, None)
-                reactor.shutdown('None')
+                self.reactor.write(f, None)
+                self.reactor.shutdown('bad')
             except TypeError:
                 pass
-            reactor.shutdown('ok')
-        self.assertEquals(self.singleCoroutineRun(f), 'ok')
+            self.reactor.shutdown('ok')
+        self.reactor.createtasklet(f)
+        self.assertEquals(self.reactor.run(), 'ok')
 
     def testImmediateStop(self):
         """Test that the reactor cleanly exits upon an immediate shutdown-command."""
-        def f(reactor):
-            reactor.shutdown(42)
-        try:
-            co = coroutine.Coroutine(f)
-            self.reactor.addCoroutine(co, (self.reactor,))
-            self.reactor.run()
-            self.assertEquals(self.reactor.result, 42, "return value is wrong")
-        except coroutine.CoroutineFailed, ex:
-            raise ex[0].exc_info[0], ex[0].exc_info[1], ex[0].exc_info[2]
+        def f():
+            self.reactor.shutdown(42)
+        self.reactor.createtasklet(f)
+        self.assertEquals(self.reactor.run(), 42)
         
-    def testDelayedStop(self):
-        """Test that the reactor cleanly exits upon delayed shutdown-command."""
-        def f(reactor):
+    def testSleep(self):
+        """Test that the reactor can sleep; and does so for the right
+        amount of time."""
+        def f():
             started = time.time()
-            reactor.sleep(0.5)
+            self.reactor.sleep(0.5)
             stopped = time.time()
-            assert stopped - started >= 0.45
-            reactor.shutdown(42)
-        try:
-            co = coroutine.Coroutine(f)
-            self.reactor.addCoroutine(co, (self.reactor,))
-            self.reactor.run()
-            self.assertEquals(self.reactor.result, 42, "return value is wrong")
-        except coroutine.CoroutineFailed, ex:
-            raise ex[0].exc_info[0], ex[0].exc_info[1], ex[0].exc_info[2]
+            if stopped - started >= 0.45:
+                self.reactor.shutdown(42)
+            else:
+                self.reactor.shutdown(stopped - started)
+        self.reactor.createtasklet(f)
+        self.assertEquals(self.reactor.run(), 42)
 
-    def testRefusedConnection(self):
-        def g(reactor, port):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock = reactor.prepareFile(sock)
-            try:
-                reactor.connect(sock, ('127.0.0.1', port))
-                reactor.shutdown(0)
-            except socket.error, (err, mesg):
-                if not err == errno.ECONNREFUSED:
-                    raise
-                reactor.shutdown(42)
-
-        def h(reactor):
-            reactor.sleep(1.0)
-            reactor.shutdown(1)
-
-        try:
-            port = random.random() * 8192 + 22000
-            self.reactor.addCoroutine(coroutine.Coroutine(g), (self.reactor, port))
-            self.reactor.addCoroutine(coroutine.Coroutine(h), (self.reactor,))
-            self.reactor.run()
-            self.failUnless(hasattr(self.reactor, 'result'), 'return value missing')
-            self.assertEqual(self.reactor.result, 42)
-        except coroutine.CoroutineFailed, ex:
-            raise ex[0].exc_info[0], ex[0].exc_info[1], ex[0].exc_info[2]
-
-    def testSimplePair(self):
-        """Test that the reactor works with a simple TCP client / server."""
-        def f(reactor, port):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.bind(('0.0.0.0', port))
-            sock.listen(1)
-            sock = reactor.prepareFile(sock)
-            fd, addr = reactor.accept(sock)
-            reactor.close(sock)
-            d = reactor.read(fd, 4096)
-            reactor.shutdown(d)
-            fd.close()
-
-        def g(reactor, port):
-            reactor.sleep(1)
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock = reactor.prepareFile(sock)
-            reactor.connect(sock, ('127.0.0.1', port))
-            reactor.write(sock, 'Hello, World\n')
-            reactor.close(sock)
-
-        try:
-            port = random.random() * 8192 + 22000
-            self.reactor.addCoroutine(coroutine.Coroutine(f, 'server'), (self.reactor, port))
-            self.reactor.addCoroutine(coroutine.Coroutine(g, 'client'), (self.reactor, port))
-            self.reactor.run()
-            self.failUnless(hasattr(self.reactor, 'result'), 'return value missing')
-            self.assertEqual(self.reactor.result, "Hello, World\n", "return value is wrong")
-        except coroutine.CoroutineFailed, ex:
-            raise ex[0].exc_info[0], ex[0].exc_info[1], ex[0].exc_info[2]
-
-    def testSimpleServer(self):
-        """Test that the reactor works with a simple TCP server."""
-        def f(reactor, port):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.bind(('0.0.0.0', port))
-            sock.listen(1)
-            sock = reactor.prepareFile(sock)
-            fd, addr = reactor.accept(sock)
-            reactor.close(sock)
-            d = reactor.read(fd, 4096)
-            fd.close()
-            return d
-
-        def g(reactor, port):
-            os.system('( echo "Hello, World" | ./bin/netcat localhost %d ) &' % port)
-            reactor.sleep(2.0)
-            reactor.shutdown(42)
-
-        try:
-            port = random.random() * 8192 + 22000
-            co = coroutine.Coroutine(f)
-            self.reactor.addCoroutine(co, (self.reactor, port))
-            self.reactor.addCoroutine(coroutine.Coroutine(g), (self.reactor, port))
-            self.reactor.run()
-            self.failUnless(hasattr(co, 'result'), 'return value missing')
-            self.assertEqual(co.result, "Hello, World\n", "return value is wrong")
-        except coroutine.CoroutineFailed, ex:
-            raise ex[0].exc_info[0], ex[0].exc_info[1], ex[0].exc_info[2]
-
-    def testTransport(self):
-        """Test that the reactor can working Transports."""
-        def read(reactor, file, count):
-            t = reactor.createTransport(os.open(file, os.O_RDONLY), 'file://' + file)
-            v = t.read(count)
-            t.close()
-            return v
-        def write(reactor, file, data):
-            t = reactor.createTransport(os.open(file, os.O_WRONLY), 'file://' + file)
-            t.write(data)
-            t.close()
-            return 1
-
-        readZero = coroutine.Coroutine(read)
-        readPasswd = coroutine.Coroutine(read)
-        writeNull = coroutine.Coroutine(write)
-        self.reactor.addCoroutine(readZero, (self.reactor, '/dev/zero', 1024))
-        self.reactor.addCoroutine(readPasswd, (self.reactor, '/etc/passwd', 1024))
-        self.reactor.addCoroutine(writeNull, (self.reactor, '/dev/null', 'Hello, World!'))
-        self.reactor.run()
-
-        self.assertEqual(readZero.result, '\0' * 1024)
-        self.failUnless(len(readPasswd.result))
-        self.assertEqual(writeNull.result, 1)
+#    def testRefusedConnection(self):
+#        def g(reactor, port):
+#            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#            sock = reactor.prepareFile(sock)
+#            try:
+#                reactor.connect(sock, ('127.0.0.1', port))
+#                reactor.shutdown(0)
+#            except socket.error, (err, mesg):
+#                if not err == errno.ECONNREFUSED:
+#                    raise
+#                reactor.shutdown(42)
+#
+#        def h(reactor):
+#            reactor.sleep(1.0)
+#            reactor.shutdown(1)
+#
+#        try:
+#            port = random.random() * 8192 + 22000
+#            self.reactor.addCoroutine(coroutine.Coroutine(g), (self.reactor, port))
+#            self.reactor.addCoroutine(coroutine.Coroutine(h), (self.reactor,))
+#            self.reactor.run()
+#            self.failUnless(hasattr(self.reactor, 'result'), 'return value missing')
+#            self.assertEqual(self.reactor.result, 42)
+#        except coroutine.CoroutineFailed, ex:
+#            raise ex[0].exc_info[0], ex[0].exc_info[1], ex[0].exc_info[2]
+#
+#    def testSimplePair(self):
+#        """Test that the reactor works with a simple TCP client / server."""
+#        def f(reactor, port):
+#            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#            sock.bind(('0.0.0.0', port))
+#            sock.listen(1)
+#            sock = reactor.prepareFile(sock)
+#            fd, addr = reactor.accept(sock)
+#            reactor.close(sock)
+#            d = reactor.read(fd, 4096)
+#            reactor.shutdown(d)
+#            fd.close()
+#
+#        def g(reactor, port):
+#            reactor.sleep(1)
+#            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#            sock = reactor.prepareFile(sock)
+#            reactor.connect(sock, ('127.0.0.1', port))
+#            reactor.write(sock, 'Hello, World\n')
+#            reactor.close(sock)
+#
+#        try:
+#            port = random.random() * 8192 + 22000
+#            self.reactor.addCoroutine(coroutine.Coroutine(f, 'server'), (self.reactor, port))
+#            self.reactor.addCoroutine(coroutine.Coroutine(g, 'client'), (self.reactor, port))
+#            self.reactor.run()
+#            self.failUnless(hasattr(self.reactor, 'result'), 'return value missing')
+#            self.assertEqual(self.reactor.result, "Hello, World\n", "return value is wrong")
+#        except coroutine.CoroutineFailed, ex:
+#            raise ex[0].exc_info[0], ex[0].exc_info[1], ex[0].exc_info[2]
+#
+#    def testSimpleServer(self):
+#        """Test that the reactor works with a simple TCP server."""
+#        def f(reactor, port):
+#            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#            sock.bind(('0.0.0.0', port))
+#            sock.listen(1)
+#            sock = reactor.prepareFile(sock)
+#            fd, addr = reactor.accept(sock)
+#            reactor.close(sock)
+#            d = reactor.read(fd, 4096)
+#            fd.close()
+#            return d
+#
+#        def g(reactor, port):
+#            os.system('( echo "Hello, World" | ./bin/netcat localhost %d ) &' % port)
+#            reactor.sleep(2.0)
+#            reactor.shutdown(42)
+#
+#        try:
+#            port = random.random() * 8192 + 22000
+#            co = coroutine.Coroutine(f)
+#            self.reactor.addCoroutine(co, (self.reactor, port))
+#            self.reactor.addCoroutine(coroutine.Coroutine(g), (self.reactor, port))
+#            self.reactor.run()
+#            self.failUnless(hasattr(co, 'result'), 'return value missing')
+#            self.assertEqual(co.result, "Hello, World\n", "return value is wrong")
+#        except coroutine.CoroutineFailed, ex:
+#            raise ex[0].exc_info[0], ex[0].exc_info[1], ex[0].exc_info[2]
+#
+#    def testTransport(self):
+#        """Test that the reactor can working Transports."""
+#        def read(reactor, file, count):
+#            t = reactor.createTransport(os.open(file, os.O_RDONLY), 'file://' + file)
+#            v = t.read(count)
+#            t.close()
+#            return v
+#        def write(reactor, file, data):
+#            t = reactor.createTransport(os.open(file, os.O_WRONLY), 'file://' + file)
+#            t.write(data)
+#            t.close()
+#            return 1
+#
+#        readZero = coroutine.Coroutine(read)
+#        readPasswd = coroutine.Coroutine(read)
+#        writeNull = coroutine.Coroutine(write)
+#        self.reactor.addCoroutine(readZero, (self.reactor, '/dev/zero', 1024))
+#        self.reactor.addCoroutine(readPasswd, (self.reactor, '/etc/passwd', 1024))
+#        self.reactor.addCoroutine(writeNull, (self.reactor, '/dev/null', 'Hello, World!'))
+#        self.reactor.run()
+#
+#        self.assertEqual(readZero.result, '\0' * 1024)
+#        self.failUnless(len(readPasswd.result))
+#        self.assertEqual(writeNull.result, 1)
             
 class SelectableReactorTestCase(ReactorTestCaseMixin, unittest.TestCase):
     """Test-cases for the selectable reactor."""
-    reactor = selectable
+    from sheared.reactor import selectable
+    klass = selectable.Reactor
 
 suite = unittest.makeSuite(SelectableReactorTestCase, 'test')
 
