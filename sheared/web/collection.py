ba7b7823e69784ca636a83994b1cd6a50153998c
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2003-07-21 11:59:36 +0000

    Refactor VirtualHost so it walks the file-system hierachy via getChild; also, fix both collections to handle requests without a tailing slash correctly (i.e. premanently redirect), and massage the index-finding for StaticCollection to be more natural.
    git-svn-id: file:///home/sune/furrfu/repositories/public/Sheared@192 5646265b-94b7-0310-9681-9501d24b2df7

--- sheared/web/collection.py
+++ sheared/web/collection.py
@@ -26,49 +26,46 @@ import os, mimetypes, stat, errno
 class StaticCollection:
     def __init__(self):
         self.bindings = {}
+        self.index = 'index'
 
     def bind(self, root, thing):
         self.bindings[root] = thing
 
-    def getChild(self, request, reply, path):
-        if not self.bindings.has_key(path):
-            raise error.web.NotFoundError('Resource not found.')
-        return self.bindings[path]
+    def getChild(self, request, reply, subpath):
+        if not subpath:
+            subpath = self.index
+        if not self.bindings.has_key(subpath):
+            raise error.web.NotFoundError
+        return self.bindings[subpath]
 
-    def handle(self, request, reply, path):
-        if not getattr(self, 'index', None):
-            raise error.web.ForbiddenError
-        return self.index.handle(request, reply, path)
+    def handle(self, request, reply, subpath):
+        if request.path.endswith('/'):
+            reply.headers.setHeader('Location', request.path + '/')
+            raise error.web.MovedPermanently
+        else:
+            self.getChild(request, reply, '').handle(request, reply, subpath)
 
 class FilesystemCollection:
-    def __init__(self, root, mt=None):
+    def __init__(self, root, path_info='', mt=None):
         self.root = root
+        self.path_info = path_info
         if mt:
             self.mimetypes = mt
         else:
             self.mimetypes = mimetypes.MimeTypes()
 
-    def handle(self, request, reply, subpath):
-        abs_path = path.rooted_path(self.root, subpath)
-        type, encoding = self.mimetypes.guess_type(abs_path)
-        if not type:
-            type = 'application/octet-stream'
-
+    def getChild(self, request, reply, subpath):
         try:
-            st = os.stat(abs_path)
+            st = os.stat(self.root)
             if stat.S_ISDIR(st.st_mode):
-                raise error.web.ForbiddenError
-
+                if subpath:
+                    abs_path = self.root + os.sep + subpath
+                    return FilesystemCollection(abs_path, self.path_info, self.mimetypes)
+                else:
+                    raise error.web.ForbiddenError
             elif stat.S_ISREG(st.st_mode):
-                file = reactor.open(abs_path, 'r')
-                reply.headers.setHeader('Last-Modified', http.HTTPDateTime(st.st_mtime))
-                reply.headers.setHeader('Content-Length', st.st_size)
-                reply.headers.setHeader('Content-Type', type)
-                if encoding:
-                    reply.headers.setHeader('Content-Encoding', encoding)
-                reply.sendfile(file)
-                reply.done()
-
+                path_info = self.path_info + '/' + subpath
+                return FilesystemCollection(self.root, path_info, self.mimetypes)
             else:
                 raise error.web.ForbiddenError
 
@@ -77,3 +74,49 @@ class FilesystemCollection:
                 raise error.web.NotFoundError
             else:
                 raise error.web.ForbiddenError
+
+    def handle(self, request, reply, subpath):
+        assert not subpath
+
+        try:
+            st = os.stat(self.root)
+            if stat.S_ISDIR(st.st_mode):
+                if request.path.endswith('/'):
+                    self.getChild(request, reply, '').handle(request, reply, subpath)
+                else:
+                    reply.headers.setHeader('Location', request.path + '/')
+                    raise error.web.MovedPermanently
+            elif stat.S_ISREG(st.st_mode):
+                if os.access(self.root, os.X_OK):
+                    self.handle_exec(request, reply, subpath)
+                if os.access(self.root, os.R_OK):
+                    self.stat = st
+                    self.handle_normal(request, reply, subpath)
+            else:
+                raise error.web.ForbiddenError
+
+        except OSError, ex:
+            if ex.errno == errno.ENOENT:
+                raise error.web.NotFoundError
+            else:
+                raise error.web.ForbiddenError
+
+    def handle_normal(self, request, reply, subpath):
+        type, encoding = self.mimetypes.guess_type(self.root)
+        if not type:
+            type = 'application/octet-stream'
+
+        last_modified = http.HTTPDateTime(self.stat.st_mtime)
+
+        reply.headers.setHeader('Last-Modified', last_modified)
+        reply.headers.setHeader('Content-Length', self.stat.st_size)
+        reply.headers.setHeader('Content-Type', type)
+        if encoding:
+            reply.headers.setHeader('Content-Encoding', encoding)
+
+        file = reactor.open(self.root, 'r')
+        reply.sendfile(file)
+        reply.done()
+
+    def handle_exec(self, request, reply, subpath):
+        raise error.web.ForbiddenError
