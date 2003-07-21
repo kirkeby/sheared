a5e37f92c1666b34f7870410b57bab67b4c2a4d4
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2003-07-21 18:40:30 +0000

    New process module.
    git-svn-id: file:///home/sune/furrfu/repositories/public/Sheared@203 5646265b-94b7-0310-9681-9501d24b2df7

--- sheared/python/process.py
+++ sheared/python/process.py
@@ -16,6 +16,34 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-__all__ = ['coroutine', 'fdpass', 'aio', 'daemonize', 'commands', 'io',
-           'benchmark', 'queue', 'path', 'application', 'conffile',
-           'process']
+import os, pickle, sys
+
+from sheared import reactor
+from sheared.python import io
+
+def runInChildProcess(f, args=(), kwargs={}):
+    r, w = os.pipe()
+
+    pid = os.fork()
+    if pid:
+        os.close(w)
+        rv = io.readall(reactor.openfd(r, '<%r pipe>' % f))
+        while not os.waitpid(pid, os.WNOHANG)[0] == pid:
+            reactor.sleep(1)
+
+        rv, exc = pickle.loads(rv)
+        if exc:
+            raise exc
+        else:
+            return rv
+
+    else:
+        try:
+            v = apply(f, args, kwargs), None
+        except:
+            v = None, sys.exc_info()[1]
+        os.write(w, pickle.dumps(v))
+
+        # sys.exit raises SystemExit which is caught by try/excepts deep
+        # in the bowels of the reactor core. So we do this instead:
+        os.execv("/bin/true", ["/bin/true"])
