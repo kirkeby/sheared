be77f9cea009b87aebc1b7348c5cc243f365d0fc
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2003-07-22 11:48:44 +0000

    New WebserverApplication for simple web-servers.
    git-svn-id: file:///home/sune/furrfu/repositories/public/Sheared@231 5646265b-94b7-0310-9681-9501d24b2df7

--- sheared/web/__init__.py
+++ sheared/web/__init__.py
@@ -18,4 +18,5 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 __all__ = ['server', 'subserver', 'querystring', 'virtualhost',
-           'collection', 'error', 'entwiner', 'xmlrpc', 'resource']
+           'collection', 'error', 'entwiner', 'xmlrpc', 'resource',
+           'application']
---
+++ sheared/web/application.py
@@ -0,0 +1,44 @@
+from sheared.python.application import Application
+from sheared.web.server import HTTPServer
+from sheared.web.virtualhost import VirtualHost
+from sheared import reactor
+
+webserver_options = [
+    ['set_str', 1, 'interface', '', 'interface', 'webserver.interface',
+     'Bind web server to this interface.'],
+
+    ['set_int', 1, 'port', '', 'port', 'webserver.port',
+     'Bind web server to this port.'],
+
+    ['set_str', 1, 'hostname', '', 'hostname', 'webserver.hostname',
+     'Externally visible name of web server.'],
+]
+
+class WebserverApplication(Application):
+    def __init__(self, name='webserver', options=[]):
+        self.port = 80
+        self.interface = ''
+        self.hostname = 'localhost'
+
+        opts = []
+        opts.extend(webserver_options)
+        opts.extend(options)
+
+        Application.__init__(self, name, opts)
+
+    def configure(self):
+        raise NotImplementedError
+
+    def setup(self):
+        if self.port == 80:
+            l = 'http://%s/' % self.hostname
+        else:
+            l = 'http://%s:%d/' % (self.hostname, self.port)
+        self.root = self.configure()
+        self.vhost = VirtualHost(self.root, l)
+
+        self.webserver = HTTPServer()
+        self.webserver.addVirtualHost(self.hostname, self.vhost)
+        self.webserver.setDefaultHost(self.hostname)
+
+        reactor.listenTCP(self.webserver, (self.interface, self.port))
