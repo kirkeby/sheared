4a2680dbb0624b559c82c64ff013f17085af18a8
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2003-07-22 10:31:38 +0000

    Add descriptions to Application.options; add --help and --conf-help options.
    git-svn-id: file:///home/sune/furrfu/repositories/public/Sheared@227 5646265b-94b7-0310-9681-9501d24b2df7

--- sheared/python/application.py
+++ sheared/python/application.py
@@ -28,17 +28,38 @@ from sheared.python import conffile
 from sheared import reactor
 
 app_options = [
-   [ 'set_bool',     0, 'daemonize', '',  'daemonize',  'application.daemonize' ],
-   [ 'clear_bool',   0, 'daemonize', '',  'foreground', 'application.foreground' ],
-   [ 'set_str',      1, 'logfile',   'l', 'log-file',   'application.log-file' ],
-   [ 'set_str',      1, 'pidfile',   'p', 'pid-file',   'application.pid-file' ],
-   [ 'opt_conf',     1, '',          'c', 'conf-file',  '' ],
-   [ 'opt_confstmt', 1, '',          'C', 'conf',       '' ],
+   [ 'help',         0, 'help',      '',  'help',       '',
+     'Display (this) help on command-line arguments.' ],
+     
+   [ 'help',         0, 'conf-help', '',  'conf-help','',
+     'Display help on configuration-file variables.' ],
+     
+   [ 'set_bool',     0, 'daemonize', '',  'daemonize',  'application.daemonize',
+     'Run as a UNIX daemon process (implies log-file\n'
+     'and pid-file).' ],
+     
+   [ 'clear_bool',   0, 'daemonize', '',  'foreground', 'application.foreground',
+     'Run in the foreground.' ],
+     
+   [ 'set_str',      1, 'logfile',   'l', 'log-file',   'application.log-file',
+     'Write messages to file.' ],
+     
+   [ 'set_str',      1, 'pidfile',   'p', 'pid-file',   'application.pid-file',
+     'Write pid-file.' ],
+     
+   [ 'opt_conf',     1, '',          'c', 'conf-file',  '',
+     'Read configuration file (may be given multiple\n'
+     'times).' ],
+
+   [ 'opt_confstmt', 1, '',          'C', 'conf',       '',
+     'Set one configuration-file variable from\n'
+     'command-line.' ],
 ]
 
 class Application:
     def __init__(self, name, options):
         self.name = name
+        self.description = ''
 
         self.conf_files = ['/etc/%s.conf', '~/.%s.conf']
         self.conf_files = [c % self.name for c in self.conf_files]
@@ -52,7 +73,75 @@ class Application:
         self.pidfile = None
 
         self.reactor = reactor
-        
+
+
+    def help(self, name, _):
+        if self.description:
+            print "   ", self.description
+            print "   ", "=" * len(self.description)
+            print
+        if name == 'help':
+            self.help_argv()
+        elif name == 'conf-help':
+            self.help_conf()
+        sys.exit(0)
+
+    def help_argv(self):
+        print "Usage: %s" % self.usage()
+
+        for handler, value, name, short, long, conf, description in self.options:
+            if short or long:
+                break
+        else:
+            return
+
+        print
+        print "Options:"
+        for handler, value, name, short, long, conf, description in self.options:
+            if not (short or long):
+                continue
+            if not description:
+                description = '[No description available.]'
+
+            if short:
+                s = '-%s ' % short
+            else:
+                s = '   '
+
+            if long:
+                s = s + '--%s ' % long
+                if len(long) <= 20:
+                    s = s + ' ' * (20 - len(long))
+                else:
+                    s = s + '\n' + ' ' * 26
+            else:
+                s = s + ' ' * 23
+
+            s = s + description.replace('\n', '\n' + ' ' * 28)
+            print '  ' + s
+
+    def usage(self):
+        return "%s [options]" % self.name
+
+    def help_conf(self):
+        for handler, value, name, short, long, conf, description in self.options:
+            if conf:
+                break
+        else:
+            print "Nothing to configure."
+            return
+
+        print "Available configuration-file variables:"
+        for handler, value, name, short, long, conf, description in self.options:
+            if not conf:
+                continue
+            if not description:
+                description = '[No description available.]'
+
+            print
+            print conf
+            print '  ' + description.replace('\n', '\n  ')
+
     def set_bool(self, name, value):
         assert not value
         setattr(self, name, 1)
@@ -84,7 +173,7 @@ class Application:
 
 
     def handle_confstmt(self, opt, val):
-        for handler, value, name, short, long, conf in self.options:
+        for handler, value, name, short, long, conf, description in self.options:
             if not conf:
                 continue
             if opt == conf:
@@ -98,7 +187,7 @@ class Application:
     def parse_args(self, argv):
         sopt = ''
         lopt = []
-        for handler, value, name, short, long, conf in self.options:
+        for handler, value, name, short, long, conf, description in self.options:
             if short:
                 sopt = sopt + short
                 if value:
@@ -112,7 +201,7 @@ class Application:
         opts, args = getopt.getopt(argv, sopt, lopt)
 
         for opt, val in opts:
-            for handler, value, name, short, long, conf in self.options:
+            for handler, value, name, short, long, conf, description in self.options:
                 if (opt == '-' + short) or (opt == '--' + long):
                     break
             else:
