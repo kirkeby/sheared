0471a60dc846b60438a25d5b856f8b79af98c335
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2003-08-05 12:51:30 +0000

    Refactor: Rename DumbProxy, it is now DumbProxyPass. New: Add a real HTTP proxy (new DumbProxy).
    git-svn-id: file:///home/sune/furrfu/repositories/public/Sheared@312 5646265b-94b7-0310-9681-9501d24b2df7

--- sheared/web/proxy.py
+++ sheared/web/proxy.py
@@ -18,11 +18,43 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
+import urlparse
+
 from sheared import error
 from sheared import reactor
 from sheared.python import io
 
 class DumbProxy:
+    def handle(self, request, reply):
+        try:
+            host = request.requestline.uri[1]
+            addr = host, 80
+            
+            uri = list(request.requestline.uri)
+            uri[0] = ''
+            uri[1] = ''
+
+            transport = reactor.connectTCP(addr)
+            
+            transport.write('%s %s HTTP/%d.%d\r\n' % (
+                                             request.requestline.method,
+                                             urlparse.urlunsplit(uri),
+                                             request.requestline.version[0],
+                                             request.requestline.version[1]))
+
+            for k, v in request.headers.items():
+                transport.write('%s: %s\r\n' % (k, v))
+            transport.write('\r\n')
+            transport.write(request.body)
+
+            reply.transport.sendfile(transport)
+            
+        except OSError:
+            pass
+
+        transport.close()
+
+class DumbProxyPass:
     def __init__(self, host, port):
         self.host = host
         self.port = port
