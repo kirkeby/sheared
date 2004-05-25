2661e2181126f50284d790303e0599ec227305af
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2004-05-25 10:11:30 +0000

    Read all data if no max-argument is given.
    git-svn-id: file:///home/sune/furrfu/repositories/public/Sheared@850 5646265b-94b7-0310-9681-9501d24b2df7

--- src/sheared/reactors/base.py
+++ src/sheared/reactors/base.py
@@ -80,21 +80,32 @@ class ReactorFile:
 
     def fileno(self):
         return self.fd
-    def read(self, max=4096):
-        return self.reactor._read(self, max)
+    def read(self, max=None):
+        if max is None:
+            d = ''
+            while 1:
+                r = self.read(4096)
+                if r == '':
+                    break
+                d = d + r
+
+        else:
+            d = self.reactor._read(self, max)
+    
+        return d
+        
     def write(self, data):
         self.reactor._write(self, data)
+    
     def sendfile(self, file):
         if isinstance(file, ReactorFile):
             self.reactor._sendfile(file, self)
         else:
-            while 1:
-                d = file.read()
-                if d == '':
-                    break
-                self.write(d)
+            self.write(file.read())
+
     def seek(self, o, i):
         return os.lseek(self.fd, o, i)
+
     def close(self):
         if not self.closed:
             os.close(self.fd)
