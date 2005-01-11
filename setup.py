8755cf2d3a4fb9f4d420d2d3eb8998856e280d9a
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-01-11 12:50:46 +0000

    s/wcgi/wscgi/
    git-svn-id: file:///home/sune/furrfu/repositories/public/sandbox/Sheared+new-tests@1017 5646265b-94b7-0310-9681-9501d24b2df7

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name = "Sheared", version = "0.1",
         'sheared', 'sheared.database', 'sheared.protocol',
         'sheared.python', 'sheared.reactors', 'sheared.web',
         'sheared.web.server', 'sheared.web.collections',
-        'skewed', 'skewed.wcgi', 'skewed.web',
+        'skewed', 'skewed.wsgi', 'skewed.web',
       ],
       package_dir = { '': 'src' },
       ext_modules = [
