47fa32de02a139d148ac3f80e3a731e2275c8684
sune <sune@5646265b-94b7-0310-9681-9501d24b2df7>
2005-04-15 11:43:23 +0000

    Add wsgi-httpd.
    git-svn-id: file:///home/sune/furrfu/repositories/public/trunk/Sheared@1156 5646265b-94b7-0310-9681-9501d24b2df7

--- setup.py
+++ setup.py
@@ -1,28 +1,9 @@
-# vim:syntax=python:textwidth=0
-#
-# Sheared -- non-blocking network programming library for Python
-# Copyright (C) 2003  Sune Kirkeby
-# 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-
-
+#!/usr/bin/env python
 from distutils.core import setup, Extension
-setup(name = "Sheared", version = "0.1",
+setup(name = "Sheared",
+      version = "$Version: $",
       author = "Sune Kirkeby",
-      url = "http://ibofobi.dk/stuff/sheared/",
+      url = "http://ibofobi.dk/stuff/sheared",
       packages = [
         'sheared', 'sheared.database', 'sheared.protocol',
         'sheared.python', 'sheared.reactors', 'sheared.web',
@@ -32,4 +13,5 @@ setup(name = "Sheared", version = "0.1",
       package_dir = { '': 'src' },
       package_data = { 'sheared.web': ['test-docroot/*.*',
                                        'test-docroot/sub/.empty'], },
+      scripts = ['scripts/wsgi-httpd'],
     )
